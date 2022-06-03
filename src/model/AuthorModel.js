8f0d4bb3adbd8040959ff1cdbb21c2a5675f1247
anilraj86 <anilaryaa1006@gmail.com>
2022-06-03 15:06:58 +0400

    db path

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+//mongoose.connect('mongodb://localhost:27017/Library');
+const dbpath="mongodb+srv://anilfsd:Csfsd@123@cluster0.hlyik.mongodb.net/?retryWrites=true&w=majority/Library";
+mongoose.connect(dbpath);
+
 const Schema = mongoose.Schema;
 
 
